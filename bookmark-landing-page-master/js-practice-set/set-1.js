b678add5578436cd836c689d357633f0873d95c0
Hozaifa Uzair <106414792+devhoz@users.noreply.github.com>
2023-01-29 20:05:39 +0530

    Add files via upload

---
+++ bookmark-landing-page-master/js-practice-set/set-1.js
@@ -0,0 +1,44 @@
+// Practice que 1
+let a;
+a="practice-set1";
+let b;
+b=5;
+result=(a+b);
+console.log(result)
+
+// Practice que 2
+
+console.log(typeof(result));
+
+// Practice que 3
+
+const c= {
+    name:"Anurag",
+    class:"III-B",
+    isFemale:false,
+    isIndian:true,
+}
+
+
+console.log(c)
+
+// Practice que 4
+
+c['Percentage']= "89.6%";
+c['ClassTeacher']="ChintaMani"
+console.log(c)
+// *******We can add values to the objects using object refrence, Whereas Object refrence such as "c" in this case cannot be re-degfined with the same name. Refrence can only be use to add or modify Values inside.********
+
+// Practice-que-5
+
+const dictionary={
+    Hello: "A way to greet people",
+    Greedy:"Laalchi for some instanse",
+    CodeingProgram:"Set of instruction given to a computer to work on a given task",
+    Path:"A defined way of achieving some task",
+} 
+
+console.log (dictionary.Path)
+console.log(dictionary['Hello'])
+// ****** Value in object can be accessed with both syntaxt mentioned******
+
---
+++ bookmark-landing-page-master/js-practice-set/set-2.js
@@ -0,0 +1,11 @@
+// Find a person age lie between 20 && 50
+
+let age=prompt("What is your age, buddy?");
+
+if (age>20 && age<50){
+    console.log(alert("Kudois!!He is eleigible to go for trip!"));
+}
+
+else {
+    console.log(alert("Sorry, you're below 20 of age. You'll need your parents nconsent for the same."));
+}
